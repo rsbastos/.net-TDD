b94f61306bc87d07f4b11295a5023f48aa941898
rsbastos <renedossantosbastos@gmail.com>
2024-09-16 13:35:51 -0300

    TDD project

---
+++ New Talents/New Talents/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewTalents
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+        private string data;
+
+        public Calculadora(string data)
+        {
+            listaHistorico = new List<string>();
+            this.data = data;
+        }
+
+        public int somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public int subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public int multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public int dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public List<string> historico()
+        {
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
+        }
+    }
+}
---
+++ New Talents/Teste New Talents/UnitTest1.cs
@@ -0,0 +1,89 @@
+using NewTalents;
+using System;
+using Xunit;
+
+namespace TesteNewTalents
+{
+    public class UnitTest1
+    {
+        public Calculadora construirClasse()
+        {
+            string data = "16/09/2024";
+            Calculadora calc = new Calculadora(data);
+
+            return calc;
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 9)]
+        public void TesteSomar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.somar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        public void TesteMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.multiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(5, 5, 1)]
+        public void TesteDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.dividir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 4)]
+        [InlineData(5, 5, 0)]
+        public void TesteSubtrair(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = construirClasse();
+
+            Assert.Throws<DivideByZeroException>(() => calc.dividir(3, 0));
+        }
+
+        [Fact]
+        public void TestarHistorico()
+        {
+            Calculadora calc = construirClasse();
+
+            calc.somar(1, 2);
+            calc.somar(3, 2);
+            calc.somar(1, 4);
+            calc.somar(6, 2);
+
+            var lista = calc.historico();
+
+            Assert.NotEmpty(calc.historico());
+            Assert.Equal(3, lista.Count);   
+        }
+    }
+}
